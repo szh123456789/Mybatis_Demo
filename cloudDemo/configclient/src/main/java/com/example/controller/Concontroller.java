c9b3b9f27c361f0a8cb9e634134bc6b5adfbc040
szh123456789 <1696646098@qq.com>
2021-10-19 23:20:13 +0800

    cloudgit配置文件

---
+++ cloudDemo/configclient/src/main/java/com/example/controller/Concontroller.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Concontroller {
+
+    @Value("${one}")
+    private String one;
+
+    @Value("${two}")
+    private String two;
+
+    @Value("${three}")
+    private String three;
+
+    @RequestMapping("/conn")
+    private String  configgit(){
+        return this.one;
+    }
+}
